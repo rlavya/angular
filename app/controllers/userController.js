3a061c791829a76a4a4935e5a275de325ed77f45
Lavya R <lavya@qburst.com>
2016-02-01 15:19:08 +0530

    add and edit popup

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,5 +1,13 @@
-myApp.controller('userController', function($scope, UserService) {
+myApp.controller('userController', function($scope,UserService) {
     UserService.fetchUserDetails().then(function(data) {
         $scope.data = data;
     })
-});
+    $scope.showAddForm = true;
+    $scope.ShowAdd = function () {
+        $scope.showAddForm = $scope.showAddForm ? false : true;
+    }
+    $scope.showEditForm = true;
+    $scope.ShowEdit = function () {
+        $scope.showEditForm = $scope.showEditForm ? false : true;
+    }
+   });

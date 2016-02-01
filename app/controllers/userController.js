2e86c4209406529a1f5c2fed84efa987479bd77d
Lavya R <lavya@qburst.com>
2016-02-01 18:33:05 +0530

    added user details dynamically

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -2,12 +2,27 @@ myApp.controller('userController', function($scope,UserService) {
     UserService.fetchUserDetails().then(function(data) {
         $scope.data = data;
     })
+    $scope.addRow = function(){
+        if($scope.firstname == undefined || $scope.lastname == undefined || $scope.email == undefined) {
+            alert('enter all details');
+            return false;
+        }
+	$scope.data.push({ 'firstname':$scope.firstname, 'lastname': $scope.lastname, 'email':$scope.email, 'status':'active' });
+    $scope.showAddForm = true;
+    };
+
     $scope.showAddForm = true;
     $scope.ShowAdd = function () {
         $scope.showAddForm = $scope.showAddForm ? false : true;
     }
+    $scope.HideAdd = function () {
+        $scope.showAddForm = $scope.showAddForm ? false : true;
+    }
     $scope.showEditForm = true;
-    $scope.ShowEdit = function () {
+    $scope.ShowEdit = function (e) {
+        $scope.showEditForm = $scope.showEditForm ? false : true;
+    }
+    $scope.HideEdit = function () {
         $scope.showEditForm = $scope.showEditForm ? false : true;
     }
    });

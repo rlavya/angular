5b540c5004d3a93a8492f3d5f942da02af5ffe2e
Lavya R <lavya@qburst.com>
2016-02-04 09:25:38 +0530

    edit icon

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,29 +3,38 @@ myApp.controller('userController', function($scope,UserService) {
         $scope.data = data;
     })
     $scope.addRow = function(){
-        // if($scope.firstname == undefined || $scope.lastname == undefined || $scope.email == undefined) {
-        //     alert('enter all details');
-        //     return false;
-        // }
-	$scope.data.push({ 'firstname':$scope.firstname, 'lastname': $scope.lastname, 'email':$scope.email, 'status':'active' });
-    $scope.firstname=null;
-    $scope.lastname=null;
-    $scope.email=null;
-    $scope.showAddForm = true;
+    	$scope.data.push({ 'firstname':$scope.firstname, 'lastname': $scope.lastname, 'email':$scope.add_email, 'status':'active' });
+        $scope.firstname=null;
+        $scope.lastname=null;
+        $scope.add_email=null;
+        $scope.showAddForm = true;
+        $('.shadow-div').removeClass('display_block');
     };
-
+    $scope.errorTrue = false;
+    // $('#add_form #add_email').on('keyup', function(eve){
+    //    if ($scope.form.add_email.$viewValue.length == 0) {
+    //         $scope.errorTrue = true;
+    //    }
+    //    else{
+    //         $scope.errorTrue = false;    
+    //    }
+    // });
     $scope.showAddForm = true;
     $scope.ShowAdd = function () {
+        $('.shadow-div').addClass('display_block');
         $scope.showAddForm = $scope.showAddForm ? false : true;
     }
     $scope.HideAdd = function () {
+        $('.shadow-div').removeClass('display_block');
         $scope.showAddForm = $scope.showAddForm ? false : true;
     }
     $scope.showEditForm = true;
     $scope.ShowEdit = function (e) {
+        $('.shadow-div').addClass('display_block');
         $scope.showEditForm = $scope.showEditForm ? false : true;
     }
     $scope.HideEdit = function () {
+        $('.shadow-div').removeClass('display_block');
         $scope.showEditForm = $scope.showEditForm ? false : true;
     }
    });
